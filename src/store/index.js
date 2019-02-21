861671cb35e9b5f19ec55b3b693d9e6f9f34821e
Adalberto Diniz Bueno Junior <adalberto@backbase.com>
2019-02-21 01:11:13 +0100

    Add VuexPersist npm module to provide localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersist from 'vuex-persist'
 import idea from './modules/idea'
 
 Vue.use(Vuex)
 
+const vuexLocalStorage = new VuexPersist({
+  key: 'ideas',
+  storage: window.localStorage
+})
+
 export default new Vuex.Store({
+  plugins: [vuexLocalStorage.plugin],
   modules: {
     idea
   }
